c48f5c6d285ab433163c294f750d7d241d773a98
EC2 Default User <ec2-user@ip-172-31-36-178.us-east-2.compute.internal>
2020-05-08 21:12:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+
+class Polygon {
+    constructor(array) {
+        this.arr = array
+    }
+    
+    get countSides() {
+       return this.arr.length 
+    }
+    
+    get perimeter() {
+        let perim = 0
+         for (let i = 0; i < this.arr.length; i++){
+            perim += this.arr[i]
+        }
+        return perim
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if ((this.arr[0] + this.arr[1]) > this.arr[2] && (this.arr[1] + this.arr[2]) > this.arr[0] && (this.arr[0] + this.arr[2]) > this.arr[1]) {
+            return true 
+        } 
+        
+        return false 
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        if (this.arr[0] === this.arr[1] && this.arr[1] === this.arr[2] && this.arr[2] === this.arr[3]) {
+            return true 
+        }
+        return false 
+    }
+    
+    get area() {
+        return this.arr[0] ** 2
+    }
+}
